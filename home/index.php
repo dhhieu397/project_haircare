8658abf1ca5f83169decee0c39919888fb793701
dhhieu397 <70250806+dhhieu397@users.noreply.github.com>
2022-02-21 01:48:33 +0700

    update

--- home/index.php
+++ home/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>ß
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Complete Responsive Food / Resturant Website Design Tutorial</title>
+    <title>Hair care</title>
 
     <!-- font awesome cdn link  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
